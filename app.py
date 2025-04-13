c96c287a9dd2e36915e5a61a8880c9c4814ce2da
Amisha Choudhary <103454929+Amishachoudhary22@users.noreply.github.com>
2025-04-13 18:29:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,21 +37,17 @@ def predict(model, img):
     confidence = round(100 * (np.max(predictions[0])), 2)
 
     # Call Roboflow API for segmentation
-    segmentation_result = CLIENT.infer(img, model_id="gp-dental/2")  # Replace model_id!
+    segmentation_result = CLIENT.infer(img, model_id="dental_disease_detection/2")  # Replace model_id!
     infected_area_mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
     total_area_mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
 
-    segmentation_predictions = segmentation_result.get('predictions', [])
-
-    if not segmentation_predictions:
-        st.warning("No infected areas detected!")
-    else:
+    if predicted_class != 'Healthy':
+        segmentation_predictions = segmentation_result['predictions']
         for seg_pred in segmentation_predictions:
-            if seg_pred.get('confidence', 0) > 0.4 and 'points' in seg_pred:
+            if seg_pred['confidence'] > 0.4:
                 points = seg_pred['points']
                 single_mask = create_mask_from_points(img.shape, points)
                 infected_area_mask = cv2.bitwise_or(infected_area_mask, single_mask)
-
 
     # Total mouth/dental area segmentation
     mouth_segmentation_result = CLIENT2.infer(img, model_id="gp-dental/2")  # Replace model_id!
